f77f8d9abdebaf76d2ee020e30b9246823f3dd7b
stentibbing <sten.tibbing@gmail.com>
2021-11-18 08:57:21 +0200

    Refactored public javascript to support multiple list highlighting

--- public/js/nm_products-public.js
+++ public/js/nm_products-public.js
@@ -5,32 +5,28 @@
    * Funciton to highlight packages belonging to product and vice versa
    */
   $(function () {
-    $("ul.nm-products-list li.nm-product").mouseenter(function () {
-      let packages = $(this).data("packages").split(" ");
-
-      $("ul.nm-packages-list li").removeClass("highlight");
-
-      packages.forEach(curPackage => {
-        $("ul.nm-packages-list li.package-" + curPackage).addClass("highlight");
+    $(".nm-product")
+      .on("mouseenter", function () {
+        let packages = $(this).data("packages").split(" ");
+        packages.forEach((curPackage) => {
+          $('.nm-package[data-package*="' + curPackage + '"]').addClass(
+            "highlight"
+          );
+        });
+      })
+      .on("mouseleave", function () {
+        $(".nm-package").removeClass("highlight");
       });
-    });
-
-    $("ul.nm-products-list li.nm-product").mouseleave(function () {
-      $("ul.nm-packages-list li").removeClass("highlight");
-    });
-
-    $("ul.nm-packages-list li.nm-package span.nm-package-title").mouseenter(
-      function () {
-        let curPackage = $(this).parent().data("package");
-        $("ul.nm-products-list li.nm-product").removeClass("highlight");
-        $("ul.nm-products-list li.nm-product.package-" + curPackage).addClass(
+    $(".nm-package")
+      .on("mouseenter", function () {
+        let curPackage = $(this).data("package");
+        console.log(curPackage);
+        $('.nm-product[data-packages*="' + curPackage + '"]').addClass(
           "highlight"
         );
-      }
-    );
-
-    $("ul.nm-packages-list li.nm-package").mouseleave(function () {
-      $("ul.nm-products-list li.nm-product").removeClass("highlight");
-    });
+      })
+      .on("mouseleave", function () {
+        $(".nm-product").removeClass("highlight");
+      });
   });
 })(jQuery);
