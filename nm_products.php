65eb258c34a76795afd73eebc0abca492917766b
stentibbing <sten.tibbing@gmail.com>
2021-11-18 09:02:55 +0200

    Updated version number

--- nm_products.php
+++ nm_products.php
@@ -9,7 +9,7 @@
  * Plugin Name:       Nordic Milk Products
  * Plugin URI:        https://www.taifuun.ee
  * Description:       This plugin adds product post type with product type and package taxonomy
- * Version:           1.1.2
+ * Version:           1.2.0
  * Author:            Taifuun OÜ
  * Author URI:        https://www.taifuun.ee
  * License:           GPL-2.0+
@@ -20,47 +20,48 @@
  */
 
 // If this file is called directly, abort.
-if ( ! defined( 'WPINC' ) ) {
-	die;
+if (! defined('WPINC')) {
+    die;
 }
 
 /**
  * Currently plugin version.
  */
-define( 'NM_PRODUCTS_VERSION', '1.1.2' );
+define('NM_PRODUCTS_VERSION', '1.2.0');
 
 /**
  * Plugin Activation code
  */
-function activate_nm_products() {
-	require_once plugin_dir_path( __FILE__ ) . 'includes/class-nm_products-activator.php';
-	NM_Products_Activator::activate();
+function activate_nm_products()
+{
+    require_once plugin_dir_path(__FILE__) . 'includes/class-nm_products-activator.php';
+    NM_Products_Activator::activate();
 }
 
 /**
  * Plugin deactivation code
  */
-function deactivate_nm_products() {
-	require_once plugin_dir_path( __FILE__ ) . 'includes/class-nm_products-deactivator.php';
-	NM_Products_Deactivator::deactivate();
+function deactivate_nm_products()
+{
+    require_once plugin_dir_path(__FILE__) . 'includes/class-nm_products-deactivator.php';
+    NM_Products_Deactivator::deactivate();
 }
 
-register_activation_hook( __FILE__, 'activate_nm_products' );
-register_deactivation_hook( __FILE__, 'deactivate_nm_products' );
+register_activation_hook(__FILE__, 'activate_nm_products');
+register_deactivation_hook(__FILE__, 'deactivate_nm_products');
 
 /**
  * Core for setting up i18n, hooks etc.
  */
-require plugin_dir_path( __FILE__ ) . 'includes/class-nm_products.php';
+require plugin_dir_path(__FILE__) . 'includes/class-nm_products.php';
 
 /**
  * Instantiate the plugin
  */
-function run_nm_products() {
-
-	$plugin = new NM_Products();
-	$plugin->run();
-
+function run_nm_products()
+{
+    $plugin = new NM_Products();
+    $plugin->run();
 }
 
 run_nm_products();
